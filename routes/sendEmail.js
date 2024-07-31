c5c9d6260a3b75272341af7e6f253307c65f45b2
krupaGetMax <krupa@getmax.ae>
2024-07-31 18:10:30 +0400

    sss

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -9,6 +9,7 @@ const router = express.Router();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
+  
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
